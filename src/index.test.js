5180c14e463259dc140cf0641a67299b9085d591
karimad <kmehaleb@gmail.com>
2021-12-20 17:47:19 +0100

    Initial commit

---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+/**
+Given a string as an input, create a javascript 
+function to reverse the string. For example, 
+
+input:  “a”    output :  “a”
+      “abc”          “cba”
+javascript:
+
+}
+ */
+
+const reverse = (inputString) => {
+  return "";
+};
+
+it("works", () => {
+  expect(1 + 1).toBe(2);
+});
+
+it("should reverse", () => {
+  // TODO
+});
